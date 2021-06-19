5d9d0b270022286adc7da4c6ee6aa0aa09911470
Khalil <khalil.chettaoui06@gmail.com>
2021-06-19 16:47:02 +0100

    removing

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -7,16 +7,28 @@ export default class TodoList extends Component {
     super(props);
 
     this.state = {
-      todos: [{ task: "Walk The Fish" }, { task: "Groom Chickens" }],
+      todos: [],
     };
     this.create = this.create.bind(this);
+    this.remove = this.remove.bind(this);
   }
   create(newTodo) {
     this.setState({ todos: [...this.state.todos, newTodo] });
   }
-
+  remove(id) {
+    this.setState({
+      todos: this.state.todos.filter((t) => t.id !== id),
+    });
+  }
   render() {
-    const todos = this.state.todos.map((todo) => <Todo task={todo.task} />);
+    const todos = this.state.todos.map((todo) => (
+      <Todo
+        key={todo.id}
+        id={todo.id}
+        task={todo.task}
+        removeTodo={this.remove}
+      />
+    ));
     return (
       <div>
         <h1>Todo List!</h1>
